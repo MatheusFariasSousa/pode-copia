eb330b0893b79207c7eafa72a7003d3e235da506
Matheus Farias de Sousa <2301841@SALAS.AULAS>
2024-03-27 22:28:09 -0300

    novo

--- app/routes/usuario_routes.py
+++ app/routes/usuario_routes.py
@@ -22,3 +22,39 @@ def add_user(request:UsuariosSchema, db: Session = Depends(get_db)):
         db.commit()
         db.refresh(user_on_db_)
         return user_on_db_
+
+
+@router.get("/listar_todos", response_model=list[SetorResponse])
+def find_all(db: Session = Depends(get_db)):
+    setores = SetorRepository.find_all(db)
+    return [SetorResponse.from_orm(setor) for setor in setores]
+
+
+@router.get("/procurar_por_id/{id}", response_model=SetorResponse)
+def find_by_id(id: int, db: Session = Depends(get_db)):
+    setor = SetorRepository.find_by_id(db, id)
+    if not setor:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Setor não encontrado"
+        )
+    return SetorResponse.from_orm(setor)
+
+
+@router.delete("/delete/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_by_id(id: int, db: Session = Depends(get_db)):
+    if not SetorRepository.exists_by_id(db, id):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Setor não encontrado"
+        )
+    SetorRepository.delete_by_id(db, id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@router.put("/update/{id}", response_model=SetorResponse)
+def update(id: int, request: SetorRequest, db: Session = Depends(get_db)):
+    if not SetorRepository.exists_by_id(db, id):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Setor não encontrado"
+        )
+    setor = SetorRepository.save(db, SetorModel(id=id, **request.dict()))
+    return SetorResponse.from_orm(setor)
